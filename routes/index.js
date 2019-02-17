aa83e7f863e94e973df19a4a604541c14fc2303c
Margal <mgalloperalta@gmail.com>
2019-02-17 17:59:53 +0100

    Add post signup route that takes inputs from the req body. Checks if fields are empty or if user already exists in DB. If no error creates user. Redirects user to Start page

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
+
+const saltRounds = 10;
 
 const router = express.Router();
 
@@ -14,7 +18,34 @@ router.get('/login', (req, res, next) => {
 
 // GET SIGNUP PAGE
 router.get('/signup', (req, res, next) => {
-  res.render('auth/signup');
+  res.render('auth/signup', { errorMessage: undefined });
+});
+
+// POST SIGNUP FORM
+router.post('/signup', (req, res, next) => {
+  const { username, password } = req.body;
+
+  if (username === '' || password === '') {
+    return res.render('auth/signup', { errorMessage: 'no empty fields' });
+  }
+  User.findOne({ username })
+    .then((user) => {
+      if (user) {
+        res.render('auth/signup', { errorMessage: 'user already exists' });
+      } else {
+        const salt = bcrypt.genSaltSync(saltRounds);
+        const hashedPassword = bcrypt.hashSync(password, salt);
+        User.create({ username, password: hashedPassword })
+          .then(() => {
+            res.redirect('/users');
+          }).catch((error) => {
+            next(error);
+          });
+      }
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
 
 module.exports = router;
