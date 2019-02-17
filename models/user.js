c3c647df970080e30d353f7f8563f0244543e8d8
Margal <mgalloperalta@gmail.com>
2019-02-17 17:58:01 +0100

    delete name, lastname, and birthday fields

--- models/user.js
+++ models/user.js
@@ -3,9 +3,6 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  name: { type: String, required: true },
-  lastName: { type: String, required: true },
-  birthday: { type: Date, required: true },
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 }, { timestamps: true });
