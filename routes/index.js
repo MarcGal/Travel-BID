2f1b4bd9052a86c8fad61da87e6a51a8aba02099
Margal <mgalloperalta@gmail.com>
2019-02-18 16:28:14 +0100

    Add login post route, and logout route

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,33 @@ router.get('/', (req, res, next) => {
 
 // GET LOGIN PAGE
 router.get('/login', (req, res, next) => {
-  res.render('auth/login');
+  res.render('auth/login', { errorMessage: undefined });
+});
+
+// POST LOGIN FORM
+router.post('/login', (req, res, next) => {
+  const { username, password } = req.body; 
+  if (username === '' || password === '') {
+    return res.render('auth/login', { errorMessage: 'no empty fields' });
+  }
+
+  User.findOne({ username })
+    .then((user) => {
+      if (!user) {
+        res.render('auth/login', { errorMessage: 'The username does not exist.' });
+        return;
+      }
+      if (bcrypt.compareSync(password, user.password)) {
+        // Save the login in the session!
+        req.session.currentUser = user;
+        res.redirect('/users');
+      } else {
+        res.render('auth/login', { errorMessage: 'Incorrect password' });
+      }
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
 
 // GET SIGNUP PAGE
@@ -46,6 +72,14 @@ router.post('/signup', (req, res, next) => {
     .catch((error) => {
       next(error);
     });
+});
+
+// LOGOUT USER
+router.get('/logout', (req, res, next) => {
+  req.session.destroy((err) => {
+    // cannot access session here
+    res.redirect('/');
+  });
 });
 
 module.exports = router;
