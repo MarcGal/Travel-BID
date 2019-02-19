20356b2d514f56367b2850b946c776282142fdcd
Margal <mgalloperalta@gmail.com>
2019-02-19 17:06:17 +0100

    Add app.use protected route at the top of the file in order to protect all routes that are only for logged in users

--- routes/users.js
+++ routes/users.js
@@ -5,9 +5,17 @@ const middlewares = require('../middlewares');
 
 const router = express.Router();
 
+router.use(middlewares.protectedRoute);
+
 /* GET users listing. */
-router.get('/', middlewares.protectedRoute, (req, res, next) => {
+router.get('/', (req, res, next) => {
   res.render('users/start');
 });
+
+// Ejemplos de referencia a utilizar cuando el usurio quiere
+// realizar operaciones CRUD sobre ofertas y pujas
+// const { name, origin, destination } = req.body;
+// const userID = req.session.currentUser._id;
+
 
 module.exports = router;
