a7c8fd90b10d0cc2b4a0d79f3c933e7b80a860bb
Álvaro Camacho <mauricio-xxi@hotmail.com>
2019-03-02 14:10:29 +0100

    Update bid acccept route new function mongoose

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -174,15 +174,8 @@ router.get('/bid/:id/accept', async (req, res, next) => {
   try {
     const bid = await Bid.findById(id);
     const offer = await Offer.findById(bid.offerID);
-    await Bid.findOneAndUpdate(id, { userID: bid.userID, offerID: bid.offerID, bidValue: bid.bidValue, bidDescription: bid.bidDescription, Status: 1 }, { new: true });
-    await Offer.findOneAndUpdate(offer.id, {
-      userID: offer.userID,
-      from: offer.from,
-      until: offer.until,
-      location: offer.location,
-      budget: offer.budget,
-      Status: 1,
-    }, { new: true });
+    await Bid.findByIdAndUpdate(id, { Status: 1 });
+    await Offer.findByIdAndUpdate(offer.id, { Status: 1 });
     console.log(bid, offer.id);
     res.redirect(`/dashboard/offer/${offer.id}`);
   } catch (error) {
