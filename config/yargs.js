c605c9847ad509db0a6be67720133d9adf6fe11f
Margal <mgalloperalta@gmail.com>
2019-03-13 20:18:52 +0100

    Delete unnecessary files.

--- config/yargs.js
+++
@@ -1,9 +0,0 @@
-const argv = require('yargs').options({
-    direccion: {
-    alias: 'd',
-    desc: 'Direccion de la ciudad para el clima',
-    demand: true
-    }
-}).argv;
-    
-module.exports = { argv };
